4f512ede9ce00428341fe45397d2a6c738e76d16
King Lemming <kinglemming@gmail.com>
2021-08-03 02:14:08 -0700

    Minor adjustments to build.gradle.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermal/cultivation/init/TCulFoods.java
+++ src/main/java/cofh/thermal/cultivation/init/TCulFoods.java
@@ -14,13 +14,13 @@ public class TCulFoods {
 
     }
 
-    public static final Food BELL_PEPPER = new Food.Builder().hunger(1).saturation(0.3F).build();
+    public static final Food BELL_PEPPER = new Food.Builder().hunger(1).saturation(0.3F).fastToEat().build();
     public static final Food CHOCOLATE_CAKE = new Food.Builder().hunger(2).saturation(0.2F).build();
     public static final Food COFFEE = new Food.Builder().hunger(0).saturation(0.2F)
             .effect(() -> new EffectInstance(SPEED, 200, 0), 1.0F)
             .setAlwaysEdible()
             .build();
-    public static final Food COFFEE_CHERRY = new Food.Builder().hunger(1).saturation(0.1F).build();
+    public static final Food COFFEE_CHERRY = new Food.Builder().hunger(1).saturation(0.1F).fastToEat().build();
     public static final Food CORN = new Food.Builder().hunger(3).saturation(0.6F).build();
     public static final Food DOUGH = new Food.Builder().hunger(2).saturation(0.3F).build();
     public static final Food EGGPLANT = new Food.Builder().hunger(2).saturation(0.3F).build();
@@ -28,9 +28,9 @@ public class TCulFoods {
             .effect(() -> new EffectInstance(FIRE_RESISTANCE, 200, 0), 1.0F)
             .setAlwaysEdible()
             .build();
-    public static final Food GREEN_BEAN = new Food.Builder().hunger(1).saturation(0.3F).build();
+    public static final Food GREEN_BEAN = new Food.Builder().hunger(1).saturation(0.3F).fastToEat().build();
     public static final Food ONION = new Food.Builder().hunger(1).saturation(0.3F).build();
-    public static final Food PEANUT = new Food.Builder().hunger(2).saturation(0.3F).build();
+    public static final Food PEANUT = new Food.Builder().hunger(2).saturation(0.3F).fastToEat().build();
     public static final Food RADISH = new Food.Builder().hunger(1).saturation(0.3F).build();
     public static final Food SPICE_CAKE = new Food.Builder().hunger(2).saturation(0.4F)
             .effect(() -> new EffectInstance(PANACEA, 100, 0), 1.0F)
